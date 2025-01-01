667e76e36c5d3c5a136c14bdad4c0e5bf14681e4
Sachin Gupta <gupta01010.sachin@gmail.com>
2025-01-01 22:10:10 +0530

    ResumeUploader Project CustomFilter Chnage

--- projects/ResumeUploader_project/CareerForge/ResumeManager/templatetags/custom_filter.py
+++ projects/ResumeUploader_project/CareerForge/ResumeManager/templatetags/custom_filter.py
@@ -3,12 +3,8 @@ register = template.Library()
 
 
 @register.filter(name = 'remove_special_char')
-
 def remove_character(value, arg):
-    print("Values : ",value)
-    print("Args : ",arg)
     for characters in arg:
-        print("Character ",characters)
         value = value.replace(characters, '')
     return value
     
